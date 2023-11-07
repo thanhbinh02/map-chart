a711529d554d60fee67c32c92dd61a3a0c5cb53a
thanhbinh.dinh <thanhbinh.dinh@estuary.solutions>
2023-11-07 17:32:32 +0700

    Example react-sample-map

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import "./App.css";
 import HighChartSelectProvince from "./components/HighChartSelectProvince";
 import { HighChartWithBarChart } from "./components/HighChartWithBarChart";
 import HighChartWithPieProvince from "./components/HighChartWithPieProvince";
 import HighChartWithPieRegion from "./components/HighChartWithPieRegion";
+import ReactMapSimpleExample from "./components/ReactMapSimpleExample";
+
+import { Tooltip } from "react-tooltip";
 
 function App() {
   return (
@@ -25,6 +29,15 @@ function App() {
       <div className="mt-5">
         <p>Biểu đồ Pie Chart ứng với từng tỉnh</p>
         <HighChartWithPieProvince />
+      </div>
+
+      <div>
+        <ReactMapSimpleExample />
+        <Tooltip
+          // Don't forget the `#`!
+          anchorSelect="#my-anchor-element-id"
+          content="Hello world!"
+        />
       </div>
     </div>
   );
